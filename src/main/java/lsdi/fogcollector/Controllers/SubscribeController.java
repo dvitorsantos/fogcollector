b2c744706aa48acc99fc4ce1ec1c1a5bc7a80eee
Vitor Santos <vitoralannl@gmail.com>
2023-07-01 17:30:24 -0300

    fix (SubscribeController) Ajuste no redirecionamento dos eventos

--- src/main/java/lsdi/fogcollector/Controllers/SubscribeController.java
+++ src/main/java/lsdi/fogcollector/Controllers/SubscribeController.java
@@ -17,30 +17,20 @@ public class SubscribeController {
     private String workerUrl = System.getenv("WORKER_URL");
     private String cloudCollectorUrl = System.getenv("CLOUDCOLLECTOR_URL");
 
-    @PostMapping("/subscribe/{target}/{inputEventType}/{outputEventType}")
-    public void subscribe(@PathVariable String target, @PathVariable String inputEventType, @PathVariable String outputEventType) {
+    @PostMapping("/subscribe/{eventType}")
+    public void subscribe(@PathVariable String eventType) {
         ObjectMapper mapper = new ObjectMapper();
 
-        mqttService.subscribe("cdpo/FOG/event/" + inputEventType, (t, message) -> {
-            switch (target) {
-                case "FOG" -> new Thread(() -> {
-                    try {
-                        Map<String, Object> event = mapper.readValue(message.getPayload(), Map.class);
-                        restTemplate.postForObject(workerUrl + "/event/" + inputEventType, event, Map.class);
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
-                    }
-                }).start();
-                case "CLOUD" -> new Thread(() -> {
-                    try {
-                        Map<String, Object> event = mapper.readValue(message.getPayload(), Map.class);
-                        mqttService.publish("cdpo/CLOUD/event/" + outputEventType, mapper.writeValueAsBytes(event));
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
-                    }
-                }).start();
-            }
-
+        System.out.println("INFO: Subscribed to cdpo/FOG/event/" + eventType);
+        mqttService.subscribe("cdpo/FOG/event/" + eventType, (t, message) -> {
+            new Thread(() -> {
+                try {
+                    Map<String, Object> event = mapper.readValue(message.getPayload(), Map.class);
+                    restTemplate.postForObject(workerUrl + "/event/" + eventType, event, Map.class);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }).start();
         });
     }
 }
